7541347790ec331a6b80c2fafd98e24bd30b09b2
Jony0101 <121823452+Jony0101@users.noreply.github.com>
2023-04-05 19:20:28 +0100

    Add files via upload

---
+++ Files/shizoval.meta.js
@@ -0,0 +1,16 @@
+/ ==UserScript==
+// @name         Easy to Celebrate 🥂🥂
+// @version      1.0.2
+// @description  Free Tanki Online Cheat for Celebrate 🥂
+// @author       Sparky
+// @match        https://*.test-eu.tankionline.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pornhub.com
+
+// @require      https://raw.githubusercontent.com/flyover/imgui-js/master/dist/imgui.umd.js
+// @require      https://raw.githubusercontent.com/flyover/imgui-js/master/dist/imgui_impl.umd.js
+
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+
+// ==/UserScript==
